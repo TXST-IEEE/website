a722615afc38a2f37c1453c155e1caefd22cdc85
Ian Lingo <lingo.ian@yahoo.com>
2026-01-28 07:18:07 -0600

    docs: changed alt texts to slider images

--- src/content/data/sliders.ts
+++ src/content/data/sliders.ts
@@ -1,5 +1,5 @@
 /**
- * SLIDER IMAGES CAROUSEL DATA
+ * SLIDER IMAGES DATA
  *
  * HOW TO ADD/MODIFY AN ARRAY:
  * 1. Add the slider image to: /public/images/slider/
@@ -15,13 +15,13 @@ import type { CarouselImage } from "@/components/ui/ImageCarousel";
 
 export const homeSliderImages: Record<string, SlideshowImage[]> = {
     homePage: [
-        {src: "/images/gallery/MikeM-IEEESpeaker-Photo-Fall2025.jpg", alt: "RAS First Meeting Group Photo"},
-        {src: "/images/gallery/IEEE-OPS-Room-Photo3.jpg", alt: "RAS First Meeting Group Photo"},
-        {src: "/images/events/Tesla-IEEETour-Photo-Fall2025.jpg", alt: "RAS First Meeting Group Photo"},
+        {src: "/images/gallery/MikeM-IEEESpeaker-Photo-Fall2025.jpg", alt: "Mike M Meeting Room Photo"},
+        {src: "/images/gallery/IEEE-OPS-Room-Photo3.jpg", alt: "OPS Meeting Photo"},
+        {src: "/images/events/Tesla-IEEETour-Photo-Fall2025.jpg", alt: "Tesla Gigafactory Tour Group Photo"},
         {src: "/images/gallery/IEEE-Volunteer-Photo.jpg", alt: "IEEE Volunteer Event Photo"},
-        {src: "/images/events/Dell-IEEETour-Photo-Spring2025.jpg", alt: "RAS First Meeting Group Photo"},
+        {src: "/images/events/Dell-IEEETour-Photo-Spring2025.jpg", alt: "Dell Tour Group Photo"},
         {src: "/images/gallery/ras/RAS-Group-Photo.jpeg", alt: "RAS Group Photo"},
-        {src: "/images/gallery/hkn/HKN-AppliedMaterials-Group-Photo.jpg", alt: "RAS First Meeting Group Photo"},
+        {src: "/images/gallery/hkn/HKN-AppliedMaterials-Group-Photo.jpg", alt: "Applied Materials Group Photo"},
         {src: "/images/gallery/IEEE-Volunteer-Group-Photo.jpg", alt: "IEEE Volunteer Event Group Photo"},
     ],
 };
@@ -34,13 +34,13 @@ export const committeeSliderImages: Record<string, CarouselImage[]> = {
         {src: "/images/gallery/ras/RAS-FirstMeeting-Group-Photo.jpeg", alt: "RAS First Meeting Group Photo"},
         {src: "/images/gallery/ras/RAS-Group-Photo.jpeg", alt: "RAS Group Photo"},
         {src: "/images/gallery/ras/RAS-FirstMeeting-Photo.jpeg", alt: "RAS First Meeting Photo"},
-        {src: "/images/gallery/ras/RAS-DianeWelker-Robot-Photo.jpeg", alt: "RAS Diane Welker Holding Robot Photo"},
+        {src: "/images/gallery/ras/RAS-DianeWelker-Robot-Photo.jpeg", alt: "RAS Diane Welker Robot Photo"},
         {src: "/images/gallery/ras/RAS-Room-Photo.jpg", alt: "RAS Room Photo"},
-        {src: "/images/gallery/ras/RAS-MrWelker-Robot-Photo.jpeg", alt: "RAS Mr Welker Holding Robot Photo"},
+        {src: "/images/gallery/ras/RAS-MrWelker-Robot-Photo.jpeg", alt: "RAS Mr Welker Robot Photo"},
     ],
     hkn: [
         {src: "/images/gallery/hkn/HKN-AppliedMaterials-Group-Photo.jpg", alt: "HKN Applied Materials Group Photo"},
-        {src: "/images/gallery/hkn/HKN-AppliedMaterials-Group-Photo2.jpg", alt: "HKN Applied Materials Group Photo2"},
+        {src: "/images/gallery/hkn/HKN-AppliedMaterials-Group-Photo2.jpg", alt: "HKN Applied Materials Group Photo 2"},
         {src: "/images/gallery/hkn/HKN-VDURA-Group-Photo.jpg", alt: "HKN VDURA Group Photo"},
         {src: "/images/gallery/hkn/HKN-YP-Group-Photo.jpg", alt: "HKN Young Professionals Group Photo"},
         {src: "/images/gallery/hkn/HKN-Inauguration-Group-Photo.jpg", alt: "HKN Inauguration Group Photo"},
