974751f471da73543a2c027baefd5cb8894c339e
Ian Lingo <lingo.ian@yahoo.com>
2026-02-01 12:47:34 -0600

    chore: added vercel analytics and vercel speed insights for monitoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -70,6 +73,8 @@ export default function RootLayout({
         <Navbar/>
         {children}
         <Footer/>
+        <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
